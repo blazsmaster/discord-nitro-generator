9303f6107b76751101e2180efcee9f6817e9772d
blázs <64538277+blazsmaster@users.noreply.github.com>
2021-03-22 22:39:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,70 @@
+# import
 import random
 import string
 import time
-import sys
+import sys 
 
-f = open('code_chace.txt', 'a')
+# open output file
+f = open('output.txt', 'a')
 
+# write to console
 print('How many Nitro codes would you like?')
-print ('')
-print ('')
-time.sleep(2)
-amount = int(input("Number of codes:"))
-print ('')
-time.sleep(2.5)
-print ('Generating...')
-time.sleep(2.5)
+
+# write to console
 print ('')
 
+# wait
+time.sleep(1)
+
+# amount of codes
+amount = int(
+
+    # write to console
+    input("Number of codes: ")
+)
+
+# write to console
+print ('')
+
+# wait
+time.sleep(1)
+
+# write to console
+print ('Generating...')
+
+# wait
+time.sleep(1)
+
+# write to console
+print ('')
+
+# amount
 fix = 1
+
+# generator
 while fix <= amount:
+
+    # create code
     code = ('').join(random.choices(string.ascii_letters + string.digits, k=16))
+
+    # discord url
     discord_url = "https://discord.gift/"
-    f.write('https://discord.gift/' + code + '\n')
+
+    # write to file
+    f.write(discord_url + code + '\n')
+
+    # code output
     discord_code = discord_url + code
+
+    # write to console
     print(discord_code)
+
+
+    # amount +1
     fix += 1
+
+# wait
 time.sleep(2)
+
+# write to console
 print ('')
